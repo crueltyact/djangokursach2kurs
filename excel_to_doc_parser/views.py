9672d61aecc00e50b8fb7d5d32c3acdb324af7ec
Sharkov Ivan <sharkov.agent@mail.ru>
2021-12-17 20:26:43 +0300

    Try to fix style.css error

--- excel_to_doc_parser/views.py
+++ excel_to_doc_parser/views.py
@@ -14,18 +14,18 @@ from excel_to_doc_parser.py.parser import get_info_from_excel
 
 def index(request):
     context = {}
-    path = "../excel_to_doc_parser/media/excel"
+    path = "./excel_to_doc_parser/media/excel"
     files_dict = {}
-    folder = "../excel_to_doc_parser/media/generated_files"
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('An error appear ' + str(e))
+    folder = "./excel_to_doc_parser/media/generated_files"
+    # for filename in os.listdir(folder):
+    #     file_path = os.path.join(folder, filename)
+    #     try:
+    #         if os.path.isfile(file_path) or os.path.islink(file_path):
+    #             os.unlink(file_path)
+    #         elif os.path.isdir(file_path):
+    #             shutil.rmtree(file_path)
+    #     except Exception as e:
+    #         print('An error appear ' + str(e))
     for file in listdir(path):
         if isfile(join(path, file)):
             files_dict[" ".join(file.split(',')[1][:-5].split("_")[2:])] = file
@@ -37,21 +37,25 @@ def index(request):
         context['profiles'] = profiles
     if request.method == "POST":
         context['step'] = 2
-        data = get_info_from_excel(path + "/" + files_dict[request.POST.get("profile")])
+        data = get_info_from_excel(
+            path + "/" + files_dict[request.POST.get("profile")])
         context['data'] = data
         context['profile'] = request.POST.get('profile')
         if request.POST.get('discipline'):
             context['step'] = 3
             discipline = request.POST.get('discipline')
-            doc = DocxTemplate("../excel_to_doc_parser/templates/template.docx")
+            doc = DocxTemplate(
+                "./excel_to_doc_parser/templates/template.docx")
             doc.render(data[discipline])
             for i in range(len(doc.tables)):
                 table = doc.tables[i]._tbl
                 for row in doc.tables[i].rows:
                     if len(row.cells[0].text.strip()) == 0 and len(set(row.cells)) == 1:
                         table.remove(row._tr)
-            doc.save("../excel_to_doc_parser/media/generated_files/{}.docx".format(data[discipline]['program_name']))
-            context['path'] = "../excel_to_doc_parser/media/generated_files/{}.docx".format(data[discipline]['program_name'])
+            doc.save("./excel_to_doc_parser/media/generated_files/{}.docx".format(
+                data[discipline]['program_name']))
+            context['path'] = "./excel_to_doc_parser/media/generated_files/{}.docx".format(
+                data[discipline]['program_name'])
             context['name'] = data[discipline]['program_name'] + '.docx'
     return render(request, "index.html", context)
 
@@ -64,7 +68,7 @@ def download(request):
     response = HttpResponse(content, content_type='application/vnd.openxmlformats-officedocument.wordprocessingml'
                                                   '.document')
     response['Content-Length'] = os.path.getsize(file)
-    response['Content-Disposition'] = 'attachment; filename*=utf-8\'\'{}'.format(name)
+    response['Content-Disposition'] = 'attachment; filename*=utf-8\'\'{}'.format(
+        name)
     print(response['Content-Disposition'])
     return response
-
--- parser_server/settings.py
+++ parser_server/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'parser_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['../excel_to_doc_parser/templates/html'],
+        'DIRS': ['./excel_to_doc_parser/templates/html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,14 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-STATIC_URL = '../excel_to_doc_parser/static/'
+STATIC_URL = './excel_to_doc_parser/static/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
-STATIC_ROOT = '../excel_to_doc_parser/staticfiles/'
+STATIC_ROOT = './excel_to_doc_parser/staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
