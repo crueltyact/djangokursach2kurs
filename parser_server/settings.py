9d78af7c3c2ef82ebc8c3e0757a835534b9500c9
Sharkov Ivan <sharkov.agent@mail.ru>
2021-12-18 12:26:45 +0300

    Fixed error with css
    Template for start write css code, css working!

--- parser_server/settings.py
+++ parser_server/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-STATIC_URL = './excel_to_doc_parser/static/'
+STATIC_URL = './static/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 STATIC_ROOT = './excel_to_doc_parser/staticfiles/'
