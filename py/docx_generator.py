884801bbcb6ccc0886ba38244579ad916ae250ea
Ender <sim200347@gmail.com>
2021-12-26 21:30:30 +0300

    merge updated plane parser and fix bugs after it

--- py/docx_generator.py
+++ py/docx_generator.py
@@ -1,7 +1,7 @@
 from docxtpl import DocxTemplate
 
-from py.parse_plane import get_info_from_education_plane
 from py.parser import get_info_from_excel
+from py.parser_plane import get_info_from_education_plane
 
 
 def check_number(num):
@@ -16,34 +16,24 @@ def check_number(num):
 def main():
     contexts = get_info_from_excel("../templates/09_03_01_Информатика_и_ВТ,_Матрица_ВЕБ_технологии_2020.xlsx")
     for key in contexts:
-        try:
-            context_lesson = contexts[key]
-            context_plane = get_info_from_education_plane("../templates/03-5190 - ВЕБ 2020 (1).xlsx")[key]
-            context_plane['intensity_ZET_check'] = check_number(context_plane['intensity_ZET'])
-            context_plane['intensity_hours_check'] = check_number(context_plane['intensity_hours'])
-            try:
-                context_plane['total_homework_hours_check'] = check_number(context_plane['total_homework_hours'])
-            except Exception as e:
-                print(e)
-                context_plane['total_homework_hours_check'] = 0
-            try:
-                for i, _ in enumerate(context_plane['semesters']):
-                    context_plane['semesters'][i]['ZET_check'] = check_number(context_plane['semesters'][i]['ZET'])
-                    context_plane['semesters'][i]['hours_check'] = check_number(context_plane['semesters'][i]['hours'])
-                    context_plane['semesters'][i]['homework_time_check'] = check_number(
-                        context_plane['semesters'][i]['homework_time'])
-            except Exception as e:
-                print(e)
-            doc = DocxTemplate("../templates/template.docx")
-            doc.render(dict(context_lesson, **context_plane))
-            for i in range(len(doc.tables)):
-                table = doc.tables[i]._tbl
-                for row in doc.tables[i].rows:
-                    if len(row.cells[0].text.strip()) == 0 and len(set(row.cells)) == 1:
-                        table.remove(row._tr)
-            doc.save("../generated_files/{}.docx".format(key))
-        except Exception as e:
-                print(e)
+        context_lesson = contexts[key]
+        context_plane = get_info_from_education_plane("../templates/03-5190 - ВЕБ 2020 (1).xlsx")[key]
+        context_plane['intensity_ZET_check'] = check_number(context_plane['intensity_ZET'])
+        context_plane['intensity_hours_check'] = check_number(context_plane['intensity_hours'])
+        context_plane['total_homework_hours_check'] = check_number(context_plane['total_homework_hours'])
+        for i, _ in enumerate(context_plane['courses']):
+            context_plane['courses'][i]['ZET_check'] = check_number(context_plane['courses'][i]['ZET'])
+            context_plane['courses'][i]['hours_check'] = check_number(context_plane['courses'][i]['hours'])
+            context_plane['courses'][i]['homework_time_check'] = check_number(
+                context_plane['courses'][i]['homework_time'])
+        doc = DocxTemplate("../templates/template.docx")
+        doc.render(dict(context_lesson, **context_plane))
+        for i in range(len(doc.tables)):
+            table = doc.tables[i]._tbl
+            for row in doc.tables[i].rows:
+                if len(row.cells[0].text.strip()) == 0 and len(set(row.cells)) == 1:
+                    table.remove(row._tr)
+        doc.save("../generated_files/{}.docx".format(key))
 
 
 if __name__ == '__main__':
--- py/parser_plane.py
+++ py/parser_plane.py
@@ -1,14 +1,15 @@
 import xlrd
 
+
 def get_matrix(filename):
     xls = xlrd.open_workbook(filename)
     xls = xls.sheet_by_index(0)
 
     return [
         [str(xls.cell_value(i, j)).strip() for j in range(xls.ncols)]
-         for i in range(xls.nrows)
+        for i in range(xls.nrows)
     ]
-    
+
 
 def number_to_words(n):
     less_than_ten = {1: 'первом', 2: 'втором', 3: 'третьем', 4: 'четвёртом',
@@ -35,51 +36,51 @@ def number_to_words(n):
 def hours_to_zet(z):
     h = round(z / 36, 1)
     if h == int(h):
-        
+
         return int(h)
     else:
         return h
 
-def get_courses(arr, imp_cols, met='moduls'):
 
+def get_courses(arr, imp_cols, met='moduls'):
     if met == 'practice':
         sems = [
-                to_int(el) for el in arr[imp_cols['exam']].replace(' ', '').split(',')
-            ] + [
-                to_int(el) for el in arr[imp_cols['credit']].replace(' ', '').split(',')
-            ]
-        sems = list(filter(lambda x: x!=0, sems))
+                   to_int(el) for el in arr[imp_cols['exam']].replace(' ', '').split(',')
+               ] + [
+                   to_int(el) for el in arr[imp_cols['credit']].replace(' ', '').split(',')
+               ]
+        sems = list(filter(lambda x: x != 0, sems))
         return [{
-            'semester' : number_to_words(sem),
-            'course' : number_to_words(int(round(sem / 2 + 0.1))),
-            'test' : 'экзамен' if str(sem) in arr[imp_cols['exam']] else 'зачет',
-            'hours' : to_int(arr[imp_cols['ZET']]) * 36,
-            'ZET' : arr[imp_cols['ZET']],
-            'homework_time' : 0,
+            'semester': number_to_words(sem),
+            'course': number_to_words(int(round(sem / 2 + 0.1))),
+            'test': 'экзамен' if str(sem) in arr[imp_cols['exam']] else 'зачет',
+            'hours': to_int(arr[imp_cols['ZET']]) * 36,
+            'ZET': to_int(arr[imp_cols['ZET']]),
+            'homework_time': 0,
         } for sem in sems]
 
     if met == 'elective':
         sem = to_int(arr[imp_cols['elective_sem']])
         hours = to_int(arr[imp_cols['elective_hours']])
         return [{
-            'semester' : number_to_words(sem),
-            'course' : number_to_words(int(round(sem / 2 + 0.1))),
-            'test' : 'зачет',
-            'hours' : hours,
-            'ZET' : hours_to_zet(hours),
-            'homework_time' : 0,
+            'semester': number_to_words(sem),
+            'course': number_to_words(int(round(sem / 2 + 0.1))),
+            'test': 'зачет',
+            'hours': hours,
+            'ZET': hours_to_zet(hours),
+            'homework_time': 0,
         }]
 
     # преобразуем courses к виду [[курс, часы аудиторной работы]]
-    courses = list(map(lambda el: [el[0]+1, el[1]], enumerate(arr[imp_cols['sems']::])))
-    courses = list(filter(lambda el: el[1]!='', courses))
-    courses = list(map(lambda el:[el[0], float(el[1])], courses))
+    courses = list(map(lambda el: [el[0] + 1, el[1]], enumerate(arr[imp_cols['sems']::])))
+    courses = list(filter(lambda el: el[1] != '', courses))
+    courses = list(map(lambda el: [el[0], float(el[1])], courses))
 
     courses_count = len(courses)
     all_homework = to_int(arr[imp_cols['homework']])
 
     # алгоритм расчета часов домашней работы
-    homeworks = [[sem, time/2] for sem, time in courses]
+    homeworks = [[sem, time / 2] for sem, time in courses]
     if sum([el[1] for el in homeworks]) != all_homework:
         homeworks = [[sem, time] for sem, time in courses]
     if sum([el[1] for el in homeworks]) != all_homework:
@@ -89,7 +90,7 @@ def get_courses(arr, imp_cols, met='moduls'):
         idx = [el[1] for el in courses].index(max([el[1] for el in courses]))
         homeworks[idx][1] += ost
     homeworks = dict(homeworks)
-    
+
     # создаем и заполняем массив информации о каждом семестре
     res = []
     for sem, time in courses:
@@ -97,17 +98,20 @@ def get_courses(arr, imp_cols, met='moduls'):
         res[-1]['semester'] = number_to_words(sem)
         res[-1]['course'] = number_to_words(int(round(sem / 2 + 0.1)))
         res[-1]['test'] = 'экзамен' if str(sem) in arr[imp_cols['exam']] else 'зачет'
-        
+
         res[-1]['hours'] = to_int(time)
         res[-1]['ZET'] = hours_to_zet(res[-1]['hours'])
 
         res[-1]['homework_time'] = to_int(homeworks[sem])
-        
+
     return res
 
+
 def to_int(x):
-    try: return int(float(x))
-    except: return 0
+    try:
+        return int(float(x))
+    except:
+        return 0
 
 
 def find_from_matrix(dct, matrix, idx=0):
@@ -116,39 +120,39 @@ def find_from_matrix(dct, matrix, idx=0):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if matrix[i][j] in dct.values() and not rev[matrix[i][j]] in res.keys():
-                res[ rev[matrix[i][j]] ] = i if idx == 0 else j
+                res[rev[matrix[i][j]]] = i if idx == 0 else j
     return res
+
 
 # главная функция
 def get_info_from_education_plane(filename):
-
     # получаем матрицу из файла
     matrix = get_matrix(filename)
 
     # ищем нужные координаты ячеек от которых будем отталкиваться
     imp_rows = find_from_matrix({
-        'subjects' : 'Обязательная часть',
-        'practice' : 'Б.2',
-        'elective' : 'Факультативные дисциплины',
+        'subjects': 'Обязательная часть',
+        'practice': 'Б.2',
+        'elective': 'Факультативные дисциплины',
     }, matrix, 0)
 
     imp_cols = find_from_matrix({
-        'credit'         : 'зачетов',
-        'exam'           : 'экзаменов',
-        'hours'          : 'ВСЕГО по структуре',
-        'ZET'            : 'Всего, ЗЕТ',
-        'homework'       : 'Самостоятельная работа',
-        'sems'           : 'Распределение по курсам и семестрам, ауд. час.',
-        'subjects'       : 'Обязательная часть',
-        'B.1'            : 'Б.1',
-        'elective'       : 'Факультативные дисциплины',
-        'elective_sem'   : 'Семестр',
-        'elective_hours' : 'Ауд. часов',
+        'credit': 'зачетов',
+        'exam': 'экзаменов',
+        'hours': 'ВСЕГО по структуре',
+        'ZET': 'Всего, ЗЕТ',
+        'homework': 'Самостоятельная работа',
+        'sems': 'Распределение по курсам и семестрам, ауд. час.',
+        'subjects': 'Обязательная часть',
+        'B.1': 'Б.1',
+        'elective': 'Факультативные дисциплины',
+        'elective_sem': 'Семестр',
+        'elective_hours': 'Ауд. часов',
     }, matrix, 1)
 
     # преобразуем названия дисциплин к нормальному виду
     for i in range(len(matrix))[imp_rows['subjects']::]:
-        matrix[i][imp_cols['subjects']] =  matrix[i][imp_cols['subjects']].split('*')[0].strip()
+        matrix[i][imp_cols['subjects']] = matrix[i][imp_cols['subjects']].split('*')[0].strip()
 
     # создаем и заполняем выходную структуру дисциплинами из блока "Модули"
     data = {}
@@ -176,7 +180,7 @@ def get_info_from_education_plane(filename):
             data[key]['courses'] = get_courses(matrix[i], imp_cols, met='practice')
 
     # заполняем выходную структуру дисциплинами из блока "Факультативные дисциплины"
-    i = imp_rows['elective']+1
+    i = imp_rows['elective'] + 1
     while matrix[i][imp_cols['elective']] != '':
         key = matrix[i][imp_cols['elective']]
 
