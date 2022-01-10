e6cbdc891e4ac45b5f8a45c34af5aa82b9f1a6f4
Ender <sim200347@gmail.com>
2022-01-10 16:45:05 +0300

    Merge updated parser and fix bug

--- py/parser_plane.py
+++ py/parser_plane.py
@@ -98,8 +98,8 @@ def get_courses(arr, imp_cols, met='moduls'):
         res[-1]['course'] = number_to_words(int(round(sem / 2 + 0.1)))
         res[-1]['test'] = 'экзамен' if str(sem) in arr[imp_cols['exam']] else 'зачет'
         res[-1]['homework_time'] = to_int(homeworks[sem])
-        res[-1]['hours'] = to_int(time)
-        res[-1]['ZET'] = hours_to_zet(to_int(res[-1]['hours']) + res[-1]['homework_time'])
+        res[-1]['hours'] = to_int(time) + res[-1]['homework_time']
+        res[-1]['ZET'] = hours_to_zet(to_int(res[-1]['hours']))
 
     return res
 
@@ -134,22 +134,22 @@ def get_info_from_education_plane(filename):
     }, matrix, 0)
 
     imp_cols = find_from_matrix({
-        'credit'         : 'зачетов',
-        'exam'           : 'экзаменов',
-        'hours'          : 'ВСЕГО по структуре',
-        'ZET'            : 'Всего, ЗЕТ',
-        'homework'       : 'Самостоятельная работа',
-        'sems'           : 'Распределение по курсам и семестрам, ауд. час.',
-        'subjects'       : 'Обязательная часть',
-        'B.1'            : 'Б.1',
-        'elective'       : 'Факультативные дисциплины',
-        'elective_sem'   : 'Семестр',
-        'elective_hours' : 'Ауд. часов',
+        'credit': 'зачетов',
+        'exam': 'экзаменов',
+        'hours': 'ВСЕГО по структуре',
+        'ZET': 'Всего, ЗЕТ',
+        'homework': 'Самостоятельная работа',
+        'sems': 'Распределение по курсам и семестрам, ауд. час.',
+        'subjects': 'Обязательная часть',
+        'B.1': 'Б.1',
+        'elective': 'Факультативные дисциплины',
+        'elective_sem': 'Семестр',
+        'elective_hours': 'Ауд. часов',
     }, matrix, 1)
 
     # преобразуем названия дисциплин к нормальному виду
     for i in range(len(matrix))[imp_rows['subjects']::]:
-        matrix[i][imp_cols['subjects']] =  matrix[i][imp_cols['subjects']].split('*')[0].strip()
+        matrix[i][imp_cols['subjects']] = matrix[i][imp_cols['subjects']].split('*')[0].strip()
 
     # создаем и заполняем выходную структуру дисциплинами из блока "Модули"
     data = {}
@@ -168,16 +168,14 @@ def get_info_from_education_plane(filename):
     for i in range(imp_rows['practice'], imp_rows['elective']):
         if matrix[i][imp_cols['B.1']] == '':
             key = matrix[i][imp_cols['subjects']]
-
             data[key] = {}
-
             data[key]['intensity_ZET'] = to_int(matrix[i][imp_cols['ZET']])
             data[key]['intensity_hours'] = data[key]['intensity_ZET'] * 36
             data[key]['total_homework_hours'] = 0
             data[key]['courses'] = get_courses(matrix[i], imp_cols, met='practice')
 
     # заполняем выходную структуру дисциплинами из блока "Факультативные дисциплины"
-    i = imp_rows['elective']+1
+    i = imp_rows['elective'] + 1
     while matrix[i][imp_cols['elective']] != '':
         key = matrix[i][imp_cols['elective']]
 
