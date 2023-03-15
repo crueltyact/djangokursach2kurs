c94b9d6f4345a1bcae297368552fd6efdb338c29
Ender <sim200347@gmail.com>
2023-03-15 16:32:37 +0300

    Add allowed_hosts setting

--- parser_server/settings.py
+++ parser_server/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('TRUSTED_HOST')]
 
 
 # Application definition
